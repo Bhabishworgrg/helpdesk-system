a0764e1fe6ec50a39efa83d459fc0c3f322b7d7e
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-25 11:06:39 +0545

    Initial Commit

---
+++ modules/helpdesk/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Helpdesk System',
+    'version': '0.1',
+    'depends': [
+        'base_setup'
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'GPL-3',
+}
