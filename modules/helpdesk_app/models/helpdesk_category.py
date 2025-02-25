37ff3510da15d509653e36c43043565d30a3455e
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-25 15:08:13 +0545

    Disable deleting prepopulated category

--- modules/helpdesk_app/models/helpdesk_category.py
+++ modules/helpdesk_app/models/helpdesk_category.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models
-
+from odoo.exceptions import UserError
 
 class HelpdeskCategory(models.Model):
     _name = 'helpdesk_app.helpdesk_category'
@@ -11,3 +11,9 @@ class HelpdeskCategory(models.Model):
     sequence = fields.Integer('Sequence', default=1)
     name = fields.Char('Name', required=True)
     description = fields.Text('Description')
+    is_prepopulated = fields.Boolean('Is Prepopulated', default=False)
+
+    def unlink(self):
+        if self.search_count([('id', 'in', self.ids), ('is_prepopulated', '=', True)]):
+            raise UserError('You cannot delete a prepopulated category.')
+        return super().unlink()
