2e234a1514c7154cad7ebff75ee8d6afcb1d8fd7
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-26 17:22:41 +0545

    Update remarks tab on restore and send buttons

--- modules/helpdesk_app/wizard/helpdesk_ticket_restore_wizard.py
+++ modules/helpdesk_app/wizard/helpdesk_ticket_restore_wizard.py
@@ -10,6 +10,12 @@ class HelpdeskTicketRestoreWizard(models.TransientModel):
     user_id = fields.Many2one('res.users', string='User Responsible')
     ticket_id = fields.Many2one('helpdesk_app.helpdesk_ticket', string='Helpdesk Ticket', required=True)
     remarks = fields.Text('Remarks', required=True)
+    remark_id = fields.Many2one('helpdesk_app.helpdesk_remark', string='Remark')
 
     def action_confirm(self):
         self.ticket_id.stage_id = self.env['helpdesk_app.helpdesk_stage'].search([('sequence', '=', 1)])
+        self.remark_id = self.env['helpdesk_app.helpdesk_remark'].create({
+            'ticket_id': self.ticket_id.id,
+            'remark': self.remarks,
+            'date_time': fields.Datetime.now(),
+        })
--- modules/helpdesk_app/wizard/helpdesk_ticket_send_wizard.py
+++ modules/helpdesk_app/wizard/helpdesk_ticket_send_wizard.py
@@ -10,6 +10,11 @@ class HelpdeskTicketSendWizard(models.TransientModel):
     user_id = fields.Many2one('res.users', string='User Responsible')
     ticket_id = fields.Many2one('helpdesk_app.helpdesk_ticket', string='Helpdesk Ticket', required=True)
     remarks = fields.Text('Remarks', required=True)
+    remark_id = fields.Many2one('helpdesk_app.helpdesk_remark', string='Remark')
 
     def action_confirm(self):
-        pass
+        self.remark_id = self.env['helpdesk_app.helpdesk_remark'].create({
+            'ticket_id': self.ticket_id.id,
+            'remark': self.remarks,
+            'date_time': fields.Datetime.now(),
+        })
