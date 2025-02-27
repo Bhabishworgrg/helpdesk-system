b17fef62ec745b6f6bab9119bdc009a22673a9d4
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-27 15:34:03 +0545

    Fix todo issues

--- modules/todo_app/models/todo_task.py
+++ modules/todo_app/models/todo_task.py
@@ -13,7 +13,7 @@ class TodoTask(models.Model):
     date_deadline = fields.Date('Date Deadline')
     is_complete = fields.Boolean('Is Completed', default=False)
     user_id = fields.Many2one('res.users', string='User Assigned')
-    todo_id = fields.Many2one('todo_app.todo', string='Todo', required=True)
+    todo_id = fields.Many2one('todo_app.todo', string='Todo', required=True, ondelete='cascade')
     remark = fields.Text('Remark')
     
     def action_mark_completed(self, remark):
