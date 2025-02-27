b5581314dec63b5014aefeed304d7f0fa61e024f
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-27 14:47:04 +0545

    Refactor remark fields

--- modules/helpdesk_app/wizard/helpdesk_ticket_restore_wizard.py
+++ modules/helpdesk_app/wizard/helpdesk_ticket_restore_wizard.py
@@ -9,13 +9,13 @@ class HelpdeskTicketRestoreWizard(models.TransientModel):
 
     user_id = fields.Many2one('res.users', string='User Responsible')
     ticket_id = fields.Many2one('helpdesk_app.helpdesk_ticket', string='Helpdesk Ticket', required=True)
-    remarks = fields.Text('Remarks', required=True)
+    remark = fields.Text('Remarks', required=True)
     remark_id = fields.Many2one('helpdesk_app.helpdesk_remark', string='Remark')
 
     def action_confirm(self):
         self.ticket_id.stage_id = self.env['helpdesk_app.helpdesk_stage'].search([('sequence', '=', 1)])
         self.remark_id = self.env['helpdesk_app.helpdesk_remark'].create({
             'ticket_id': self.ticket_id.id,
-            'remark': self.remarks,
+            'remark': self.remark,
             'date_time': fields.Datetime.now(),
         })
--- modules/helpdesk_app/wizard/helpdesk_ticket_send_wizard.py
+++ modules/helpdesk_app/wizard/helpdesk_ticket_send_wizard.py
@@ -9,12 +9,12 @@ class HelpdeskTicketSendWizard(models.TransientModel):
 
     user_id = fields.Many2one('res.users', string='User Responsible')
     ticket_id = fields.Many2one('helpdesk_app.helpdesk_ticket', string='Helpdesk Ticket', required=True)
-    remarks = fields.Text('Remarks', required=True)
+    remark = fields.Text('Remarks', required=True)
     remark_id = fields.Many2one('helpdesk_app.helpdesk_remark', string='Remark')
 
     def action_confirm(self):
         self.remark_id = self.env['helpdesk_app.helpdesk_remark'].create({
             'ticket_id': self.ticket_id.id,
-            'remark': self.remarks,
+            'remark': self.remark,
             'date_time': fields.Datetime.now(),
         })
