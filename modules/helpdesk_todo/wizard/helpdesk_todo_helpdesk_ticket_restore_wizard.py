a00ff92826db9f58ea591c04dd8ab2c1ee359c81
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-28 13:55:06 +0545

    Apply next stage button implementation

--- modules/helpdesk_todo/wizard/helpdesk_todo_helpdesk_ticket_restore_wizard.py
+++ modules/helpdesk_todo/wizard/helpdesk_todo_helpdesk_ticket_restore_wizard.py
@@ -6,6 +6,6 @@ from odoo import models, fields
 class HelpdeskTicketRestoreWizard(models.TransientModel):
     _inherit = 'helpdesk_app.helpdesk_ticket_restore_wizard'
 
-    def action_confirm(self):
-        self.ticket_id.stage_id = self.env['helpdesk_todo.stage'].search([('sequence', '=', 1)])
+    def action_confirm(self):        
+        self.ticket_id.todo_id.stage_id = self.ticket_id.stage_id = self.env['helpdesk_todo.stage'].search([('sequence', '=', 1)])
         return super().action_confirm()
