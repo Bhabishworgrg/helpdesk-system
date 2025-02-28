c7998d6d0355e4e96687bf7435595ae99f8bf8ad
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-28 14:13:49 +0545

    Enable chatter tracking

--- modules/helpdesk_app/models/helpdesk_ticket.py
+++ modules/helpdesk_app/models/helpdesk_ticket.py
@@ -11,22 +11,16 @@ class HelpdeskTicket(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     sequence = fields.Integer('Sequence', default=1)
-    active = fields.Boolean('Active', default=True)
-    title = fields.Char('Title', required=True)
-    query = fields.Text('Query', required=True)
+    active = fields.Boolean('Active', default=True, tracking=True)
+    title = fields.Char('Title', required=True, tracking=True)
+    query = fields.Text('Query', required=True, tracking=True)
     description = fields.Html('Description')
     reported_date = fields.Date('Reported Date', default=fields.Date.today)
-    user_id = fields.Many2one('res.users', 'Reported By', default=lambda self: self.env.user)
-    category_id = fields.Many2one('helpdesk_app.helpdesk_category', string='Category', default=lambda self: self.env.ref('helpdesk_app.helpdesk_category_1'))
-    tag_ids = fields.Many2many('helpdesk_app.helpdesk_tag', string='Tags')
+    user_id = fields.Many2one('res.users', 'Reported By', default=lambda self: self.env.user, tracking=True)
+    category_id = fields.Many2one('helpdesk_app.helpdesk_category', string='Category', default=lambda self: self.env.ref('helpdesk_app.helpdesk_category_1'), tracking=True)
+    tag_ids = fields.Many2many('helpdesk_app.helpdesk_tag', string='Tags', tracking=True)
     type_id = fields.Many2one('helpdesk_app.helpdesk_type', string='Type', default=lambda self: self.env.ref('helpdesk_app.helpdesk_type_1'))
     remark_ids = fields.One2many('helpdesk_app.helpdesk_remark', 'ticket_id', string='Remarks')
-    team_id = fields.Many2one('helpdesk_app.helpdesk_team', string='Team')
+    team_id = fields.Many2one('helpdesk_app.helpdesk_team', string='Team', tracking=True)
     team_member_ids = fields.Many2many('res.users', string='Team Members', related='team_id.member_ids')
-    team_member_id = fields.Many2one('res.users', string='Team Member', domain='[("id", "in", team_member_ids)]')
-
-#    def action_next_stage(self):
-#        next_stage = self.env['helpdesk_app.helpdesk_stage'].search([('sequence', '>', self.stage_id.sequence)], limit=1)
-#        if next_stage:
-#            self.stage_id = next_stage 
-#
+    team_member_id = fields.Many2one('res.users', string='Team Member', domain='[("id", "in", team_member_ids)]', tracking=True)
