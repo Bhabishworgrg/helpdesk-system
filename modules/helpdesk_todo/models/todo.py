6050d26b8601de7116a42bc0a57e28ff851cb631
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-28 11:32:28 +0545

    Sync the stages of helpdesk and todo

--- modules/helpdesk_todo/models/todo.py
+++ modules/helpdesk_todo/models/todo.py
@@ -7,3 +7,11 @@ class Todo(models.Model):
     _inherit = 'todo_app.todo'
 
     stage_id = fields.Many2one('helpdesk_todo.stage', string='Stage')
+    ticket_id = fields.Many2one('helpdesk_app.helpdesk_ticket', string='Ticket')
+
+    def write(self, vals):
+        if 'stage_id' in vals:
+            self.ticket_id.write({
+                'stage_id': vals['stage_id']
+            })
+        return super().write(vals)
