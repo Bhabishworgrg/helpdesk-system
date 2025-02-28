6e25c67b8dd90fbfc647a356e29ec7aa7977a232
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-28 16:48:51 +0545

    Show all users when team is empty

--- modules/helpdesk_app/models/helpdesk_ticket.py
+++ modules/helpdesk_app/models/helpdesk_ticket.py
@@ -22,5 +22,10 @@ class HelpdeskTicket(models.Model):
     type_id = fields.Many2one('helpdesk_app.helpdesk_type', string='Type', default=lambda self: self.env.ref('helpdesk_app.helpdesk_type_1'))
     remark_ids = fields.One2many('helpdesk_app.helpdesk_remark', 'ticket_id', string='Remarks')
     team_id = fields.Many2one('helpdesk_app.helpdesk_team', string='Team', tracking=True)
-    team_member_ids = fields.Many2many('res.users', string='Team Members', related='team_id.member_ids')
-    team_member_id = fields.Many2one('res.users', string='Team Member', domain='[("id", "in", team_member_ids)]', tracking=True)
+    team_member_ids = fields.Many2many('res.users', string='Team Members', compute='_compute_team_member_ids')
+    team_member_id = fields.Many2one('res.users', string='Team Member', domain='[("id", "in", team_member_ids)]')
+
+    @api.depends('team_id')
+    def _compute_team_member_ids(self):
+        for rec in self:
+            rec.team_member_ids = rec.team_id.member_ids if rec.team_id else self.env['res.users'].search([])
