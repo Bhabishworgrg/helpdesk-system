c86c4e11166fb82fc74e2d16002bd863bfe7631d
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-04 12:57:53 +0545

    Auto calculate progress and completed status in todo

--- modules/todo_app/models/todo.py
+++ modules/todo_app/models/todo.py
@@ -15,8 +15,18 @@ class Todo(models.Model):
     date_deadline = fields.Date('Deadline', default=fields.Date.today, tracking=True)
     summary = fields.Text('Summary', tracking=True)
     description = fields.Html('Description')
-    progress = fields.Float('Progress')
-    is_complete = fields.Boolean('Is Completed')
+    progress = fields.Float('Progress', compute='_compute_is_complete', store=True)
+    is_complete = fields.Boolean('Is Completed', compute='_compute_is_complete', store=True)
     user_id = fields.Many2one('res.users', string='User Assigned', tracking=True)
     task_ids = fields.One2many('todo_app.todo_task', 'todo_id', string='Tasks', tracking=True)
     remark_ids = fields.One2many('todo_app.todo_remark', 'todo_id', string='Remarks')
+    
+    @api.depends('task_ids', 'task_ids.is_complete')
+    def _compute_is_complete(self):
+        for rec in self:
+            if rec.task_ids:
+                rec.is_complete = all(rec.task_ids.mapped('is_complete'))
+                rec.progress = len(rec.task_ids.filtered('is_complete')) / len(rec.task_ids) * 100
+            else:
+                rec.is_complete = False
+                rec.progress = 0
