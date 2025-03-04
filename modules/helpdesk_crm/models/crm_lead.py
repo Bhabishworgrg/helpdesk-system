d9975f22386773ecdf7c25aff22799c94323eed1
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-04 12:58:54 +0545

    Update stage to completed when lead is won or lost

---
+++ modules/helpdesk_crm/models/crm_lead.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class Lead(models.Model):
+    _inherit = 'crm.lead'
+
+    ticket_id = fields.Many2one('helpdesk_app.helpdesk_ticket', string='Helpdesk Ticket')
+
+    def write(self, vals):
+        res = super().write(vals)
+        for rec in self:
+            if rec.stage_id == self.env.ref('crm.stage_lead4') or rec.active == False:
+                rec.ticket_id.write({
+                    'stage_id': self.env.ref('helpdesk_todo.stage_3').id,
+                })
+        return res 
