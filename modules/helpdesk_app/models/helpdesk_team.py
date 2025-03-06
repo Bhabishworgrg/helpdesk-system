ac80c80331a4dfd5c5d90b297b7b7996f43abe63
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-06 16:59:11 +0545

    Refactor helpdesk team model

--- modules/helpdesk_app/models/helpdesk_team.py
+++ modules/helpdesk_app/models/helpdesk_team.py
@@ -11,16 +11,12 @@ class HelpdeskTeam(models.Model):
     sequence = fields.Integer('Sequence', default=1)
     name = fields.Char('Name', required=True)
     leader_id = fields.Many2one('res.users', string='Leader', required=True)
-    member_ids = fields.Many2many('res.users', string='Members', required=True)
+    member_ids = fields.Many2many('res.users', string='Members')
+
+    _sql_constraints = [('name_unique', 'unique(name)', 'Team name already exists.')]
 
     @api.constrains('leader_id', 'member_ids')
     def _check_leader_is_member(self):
-        for team in self:
-            if team.leader_id not in team.member_ids:
-                raise UserError("The leader must be a member of the team.")
-
-    @api.onchange('member_ids')
-    def _onchange_member_ids(self):
-        return {
-            'domain': { 'leader_id': [('id', 'in', self.member_ids.ids)] }
-        }
+        for rec in self:
+            if rec.leader_id not in rec.member_ids:
+                raise UserError('The leader must be a member of the team.')
