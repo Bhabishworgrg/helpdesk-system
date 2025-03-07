60009051494d2574bb19d46438d2f09bb482e402
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-07 12:48:26 +0545

    Fix partner details

--- modules/helpdesk_app/models/helpdesk_ticket.py
+++ modules/helpdesk_app/models/helpdesk_ticket.py
@@ -24,39 +24,44 @@ class HelpdeskTicket(models.Model):
     team_id = fields.Many2one('helpdesk_app.helpdesk_team', string='Team', domain="[('id', 'in', team_ids)]", tracking=True)
     team_member_ids = fields.Many2many('res.users', string='Team Members', compute='_compute_team_member_ids')
     team_member_id = fields.Many2one('res.users', string='Team Member', domain='[("id", "in", team_member_ids)]', tracking=True)
-    partner_id = fields.Many2one('res.partner', string='Contact', compute='_compute_partner', readonly=False, store=True)
-    email = fields.Char(string='Email')
-    phone = fields.Char(string='Phone')
 
     @api.depends('team_member_id')
     def _compute_team_ids(self):
         for rec in self:
             rec.team_ids = self.env['helpdesk_app.helpdesk_team'].search([('member_ids', 'in', rec.team_member_id.ids)] if rec.team_member_id else [])
 
+    partner_id = fields.Many2one(
+        'res.partner',
+        string='Reporter',
+        tracking=True
+    )
+    partner_email = fields.Char(string='Reporter Email')
+    partner_phone = fields.Char(string='Reporter Phone')
+    
     @api.depends('team_id')
     def _compute_team_member_ids(self):
         for rec in self:
             rec.team_member_ids = rec.team_id.member_ids if rec.team_id else self.env['res.users'].search([])
 
-    @api.depends('email', 'phone')
-    def _compute_partner(self):
+    @api.onchange('partner_email', 'partner_phone')
+    def _onchange_partner_email_or_phone(self):
         for rec in self:
-            if not rec.env.context.get('from_onchange'):
-                rec.partner_id = self.env['res.partner'].search([
-                    ('email', '=', rec.email),
-                    ('phone', '=', rec.phone)
-                ], limit=1)
+            partner = self.env['res.partner'].search(
+                [
+                    ('email', '=', rec.partner_email),
+                    ('phone', '=', rec.partner_phone)
+                ],
+                limit=1
+            )
+            if partner:
+                rec.partner_id = partner
 
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
         for rec in self:
             if rec.partner_id:
-                rec.with_context(from_onchange=True).write({
-                    'email': rec.partner_id.email,
-                    'phone': rec.partner_id.phone,
-                })
+                rec.partner_email = rec.partner_id.email
+                rec.partner_phone = rec.partner_id.phone
             else:
-                rec.with_context(from_onchange=True).write({
-                    'email': False,
-                    'phone': False,
-                })
+                rec.partner_email = False
+                rec.partner_phone = False
